51efe4502f68d18bc001e1ffcce0b9ba2b443487
JonathanSAndrade <jonathan.andrade9.souza@hotmail.com>
2025-08-18 13:51:32 -0300

    js E hTML 18.08 UPDATE

--- scripts.js
+++ scripts.js
@@ -1,6 +1,36 @@
+const result = document.querySelector('.result')
+
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
 const playHuman = (humanchoice) => {
+    playTheGame(humanchoice, playmachine())
 
-    console.log(humanchoice)
+}
 
+const playmachine = () => {
+    const choices = ['pedra', 'papel', 'tesoura']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+
+    if (human === machine) {
+        result.innerHTML = 'Empate!'
+    } else if ((human === 'papel' && machine === 'pedra') ||
+        (human === 'pedra' && machine === 'tesoura') ||
+        (human === 'tesoura' && machine === 'papel')) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+        result.innerHTML = 'Você ganhou XD!'
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = 'Você perdeu para a CASSANDRA!'
+    }
 }
